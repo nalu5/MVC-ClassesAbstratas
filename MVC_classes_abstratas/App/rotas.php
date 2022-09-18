e5666901c0f15dc4c4d6f18e5f6e92f46bff69a2
ana luiza <99657943+nalu5@users.noreply.github.com>
2022-09-17 21:25:14 -0300

    Update rotas.php

--- MVC_classes_abstratas/App/rotas.php
+++ MVC_classes_abstratas/App/rotas.php
@@ -2,10 +2,10 @@
 
 use App\Controller\PessoaController;
 
-// Para saber mais sobre a função parse_url: https://www.php.net/manual/pt_BR/function.parse-url.php
+
 $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-// Para saber mais estrutura switch, leia: https://www.php.net/manual/pt_BR/control-structures.switch.php
+
 switch ($url) 
 {
     case '/':
@@ -13,8 +13,7 @@ switch ($url)
         break;
 
     case '/pessoa':
-        // Para saber mais sobre o Operador de Resolução de Escopo (::), 
-        // leia: https://www.php.net/manual/pt_BR/language.oop5.paamayim-nekudotayim.php
+        
         PessoaController::index();
         break;
 
@@ -29,8 +28,46 @@ switch ($url)
     case '/pessoa/delete':
         PessoaController::delete();
         break;
+        
+        
+        
+    case '/produto':
+        
+        ProdutoController::index();
+    break;
 
+    case '/produto/form':
+        ProdutoController::form();
+    break;
+
+    case '/produto/form/save':
+        ProdutoController::save();
+    break;
+
+    case '/produto/delete':
+        ProdutoController::delete();
+    break;
+        
+        
+
+    case '/categoriaproduto':
+        
+        CategoriaProdutoController::index();
+    break;
+
+    case '/categoriaproduto/form':
+        CategoriaProdutoController::form();
+    break;
+
+    case '/categoriaproduto/form/save':
+        CategoriaProdutoController::save();
+    break;
+
+    case '/categoriaproduto/delete':
+        CategoriaProdutoController::delete();
+    break;
+        
     default:
         echo "Erro 404";
-        break;
+    break;
 }
